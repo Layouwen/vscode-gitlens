c1b4c3758acf1363367063cb6e5d77997efb119d
nzaytsev <nikolai.zaytsev@gitkraken.com>
2024-11-15 11:04:31 +0700

    Opens multidiff view on open all changes from the home view

--- src/webviews/apps/plus/home/components/branch-section.ts
+++ src/webviews/apps/plus/home/components/branch-section.ts
@@ -79,6 +79,9 @@ export const headingLoaderStyles = css`
 `;
 
 export const branchCardStyles = css`
+	:host {
+		--gl-card-hover-background: color-mix(in lab, var(--vscode-sideBar-background) 100%, #fff 8%);
+	}
 	.branch-item {
 		position: relative;
 	}
@@ -158,6 +161,10 @@ export const branchCardStyles = css`
 		background-color: var(--gl-card-background);
 	}
 
+	.branch-item:hover .branch-item__actions,
+	.branch-item:focus-within .branch-item__actions {
+		background-color: var(--gl-card-hover-background);
+	}
 	.branch-item:not(:focus-within):not(:hover) .branch-item__actions {
 		display: none;
 	}
